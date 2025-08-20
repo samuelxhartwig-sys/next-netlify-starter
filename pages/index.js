a57c9a337d54d7aa94b80497b30bc800f4e98496
samuelxhartwig-sys <samuelxhartwig@gmail.com>
2025-08-19 23:37:37 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <img src="https://picsum.photos/200" />
       </main>
 
       <Footer />
